60e415e699cf64872d2b1f25133fb11a620ee8f5
idklol22 <wadhwasanjam@Gmail.com>
2025-09-25 13:04:11 +0530

    dqn commit

--- DQNonly/main.py
+++ DQNonly/main.py
@@ -3,10 +3,11 @@ import gymnasium
 import torch
 env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=True)
 obs, _ = env.reset()
-while True:
-    action = env.action_space.sample()
-    obs, reward, terminated, _, info = env.step(action)
-    if terminated:
-        break
+
+    while True:
+        action = env.action_space.sample()
+        obs, reward, terminated, _, info = env.step(action)
+        if terminated:
+            break
 
 env.close()
